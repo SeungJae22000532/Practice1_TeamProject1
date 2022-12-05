f0a3b44ab3a559136f93d14a3df519d72dd7c6a1
22000532 <22000532@handong.ac.kr>
2022-12-05 20:08:05 +0900

    Modify ContactDAO, ContactVO file and Home controller, and Create addpostform, editform, list jsp file

--- src/main/java/com/practice/contact/ContactDAO.java
+++ src/main/java/com/practice/contact/ContactDAO.java
@@ -1,4 +1,62 @@
 package com.practice.contact;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 public class ContactDAO {
+    private JdbcTemplate template;
+
+    public void setTemplate(JdbcTemplate template) {
+        this.template = template;
+    }
+
+    private final String CONTACT_INSERT = "insert into CONTACT (subject, pName, tName, pMail, tMail, zAddress, office) values (?, ?, ?, ?, ?, ?, ?)";
+    private final String CONTACT_UPDATE = "update CONTACT set subject=?, pName=?, tName=?, pMail=?, tMail=?, zAddress=?, office=? where id=?";
+    private final String CONTACT_DELETE = "delete from CONTACT where id=?";
+    private final String CONTACT_GET = "select * from CONTACT where id=?";
+    private final String CONTACT_LIST = "select * from CONTACT order by id desc";
+
+    public int insertContact(ContactVO vo) {
+        return template.update(CONTACT_INSERT,
+                new Object[]{vo.getpName(), vo.gettName(), vo.getpMail(), vo.gettMail(), vo.getzAddress(), vo.getOffice()});
+    }
+
+    public int deleteContact(int id) {
+        return template.update(CONTACT_DELETE,
+                new Object[]{id});
+    }
+
+    public int updateContact(ContactVO vo) {
+        return template.update(CONTACT_UPDATE,
+                new Object[]{vo.getpName(), vo.gettName(), vo.getpMail(), vo.gettMail(), vo.getzAddress(), vo.getOffice(), vo.getSeq()});
+    }
+
+    public ContactVO getContact(int seq) {
+        return template.queryForObject(CONTACT_GET,
+                new Object[]{seq},
+                new BeanPropertyRowMapper<ContactVO>(ContactVO.class));
+    }
+
+    public List<ContactVO> getContactList() {
+        return template.query(CONTACT_LIST, new RowMapper<ContactVO>() {
+            @Override
+            public ContactVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+                ContactVO data = new ContactVO();
+                data.setSeq(rs.getInt("seq"));
+                data.setSubject(rs.getString("subject"));
+                data.setpName(rs.getString("pName"));
+                data.settName(rs.getString("tName"));
+                data.setpMail(rs.getString("pMail"));
+                data.settMail(rs.getString("tMail"));
+                data.setzAddress(rs.getString("zAddress"));
+                data.setOffice(rs.getString("Office"));
+                return data;
+            }
+        });
+    }
 }
--- src/main/java/com/practice/contact/ContactVO.java
+++ src/main/java/com/practice/contact/ContactVO.java
@@ -1,4 +1,76 @@
 package com.practice.contact;
 
 public class ContactVO {
+    public int seq;
+    public String subject;
+    public String pName;
+    public String tName;
+    public String pMail;
+    public String tMail;
+    public String zAddress;
+    public String office;
+
+    public int getSeq() {
+        return seq;
+    }
+
+    public void setSeq(int seq) {
+        this.seq = seq;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String gettName() {
+        return tName;
+    }
+
+    public void settName(String tName) {
+        this.tName = tName;
+    }
+
+    public String getpMail() {
+        return pMail;
+    }
+
+    public void setpMail(String pMail) {
+        this.pMail = pMail;
+    }
+
+    public String gettMail() {
+        return tMail;
+    }
+
+    public void settMail(String tMail) {
+        this.tMail = tMail;
+    }
+
+    public String getzAddress() {
+        return zAddress;
+    }
+
+    public void setzAddress(String zAddress) {
+        this.zAddress = zAddress;
+    }
+
+    public String getOffice() {
+        return office;
+    }
+
+    public void setOffice(String office) {
+        this.office = office;
+    }
 }
